5f6e657ab83c4f1a398735ce0805fe64a3d33d51
Atharv625 <patilatharv625@gmail.com>
2026-01-11 16:33:52 +0530

    close feature implemented

--- Far_Away/src/App.jsx
+++ Far_Away/src/App.jsx
@@ -9,6 +9,11 @@ const messages = [
 
 const App = () => {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function close() {
+    setIsOpen(false);
+  }
 
   function nextStep() {
     if (step < messages.length) setStep(step + 1);
@@ -19,33 +24,44 @@ const App = () => {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        {[1, 2, 3].map((num) => (
-          <div key={num} className={step >= num ? "active" : ""}>
-            {num}
+    <>
+      {isOpen ? (
+        <div className="steps">
+          {/* Close button */}
+          <button onClick={close} className="close">
+            ✖
+          </button>
+
+          <div className="numbers">
+            {[1, 2, 3].map((num) => (
+              <div key={num} className={step >= num ? "active" : ""}>
+                {num}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
 
-      <p className="message">{messages[step - 1]}</p>
+          <p className="message">{messages[step - 1]}</p>
 
-      <div className="buttons">
-        <button
-          onClick={prevStep}
-          className="bg-[#7950f2] active:translate-y-1"
-        >
-          Previous
-        </button>
+          <div className="buttons">
+            <button
+              onClick={prevStep}
+              className="bg-[#7950f2] active:translate-y-1"
+            >
+              Previous
+            </button>
 
-        <button
-          onClick={nextStep}
-          className="bg-[#7950f2] active:translate-y-1"
-        >
-          Next
-        </button>
-      </div>
-    </div>
+            <button
+              onClick={nextStep}
+              className="bg-[#7950f2] active:translate-y-1"
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      ) : (
+        <button onClick={() => setIsOpen(true)}>Open</button>
+      )}
+    </>
   );
 };
 
